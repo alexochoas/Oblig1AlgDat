77ac7f0c1966081041004b8f6fd4be2a4ab5b9ef
alexochoas <31653697+alexochoas@users.noreply.github.com>
2018-09-03 13:23:10 +0200

    test

---
+++ untitled/src/Oblig1.java
@@ -0,0 +1,22 @@
+public class Oblig1 {
+
+
+
+
+    public static void main(String[] args){
+        System.out.println("hei");
+
+    }
+    
+
+    public static void maks(int[] a){
+
+
+
+
+    }
+
+
+
+
+}
